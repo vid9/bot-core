b71c295678d95c4fa610cfc26a5f7c32201ec828
vid9 <vid.cermelj@gmail.com>
2021-04-09 15:40:42 +0200

    start/end char update

--- custom_components/classla_tokenizer.py
+++ custom_components/classla_tokenizer.py
@@ -7,12 +7,10 @@ from rasa.nlu.tokenizers.spacy_tokenizer import POS_TAG_KEY
 
 import classla
 
-
 class ClasslaTokenizer(Tokenizer):
 
     def __init__(self, component_config: Dict[Text, Any] = None) -> None:
         """Construct a new tokenizer using the classla framework."""
-
         super().__init__(component_config)
         #classla.download("sl", force=True)
         self.nlp = classla.Pipeline("sl", type="standard", processors="tokenize,pos,lemma")
@@ -20,16 +18,15 @@ class ClasslaTokenizer(Tokenizer):
     def tokenize(self, message: Message, attribute: Text) -> List[Token]:
         text = message.get(attribute)
         doc = self.nlp(text)
-        stanza_tokens = []
-        for i in doc.sentences:
-            stanza_tokens += i.tokens
-
+        stanza_tokens = reduce(lambda a, b: a + b, doc.sentences).tokens
         tokens = []
         running_offset = 0
         for t in stanza_tokens:
             word_offset = text.index(t.text, running_offset)
             word_len = len(t.text)
             running_offset = word_offset + word_len
+            if t.misc == "SpaceAfter=No":
+                running_offset -= 1
             tokens.append(Token(
                 text=t.text,
                 start=word_offset,
