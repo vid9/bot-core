559cf69a1c3b1602bc1544be27ff1bee1581b371
vid9 <vid.cermelj@gmail.com>
2021-04-13 20:30:58 +0200

    reverted actions server

--- data/actions/actions.py
+++ data/actions/actions.py
@@ -755,9 +755,8 @@ class TvCurrentlyPlaying(Action):
 
         if res:
             elements = tvh.format_schedule(res)
-            print(elements)
             if tracker.get_latest_input_channel() == 'facebook':
-                dispatcher.utter_message(text=elements,elements=elements)
+                dispatcher.utter_message(text=json.dump(elements),elements=elements)
                 dispatcher.utter_message(text=f"Trenutno na programu {program_name}", elements=elements)
             else:
                 dispatcher.utter_message(text=f"Trenutno na programu {program_name}")
@@ -990,9 +989,7 @@ class ValidateListUpdateForm(FormValidationAction):
             return {"list_item": "end"}
         name = tracker.latest_message.get("text")
         current_items = tracker.get_slot("list_items")
-        # items = current_items
-        # if not current_items:
-        #     items = []
+        items = current_items
         list_name = list_helper.get_list(tracker.get_slot("list_name"), tracker.sender_id)
         if not list_name or list_name is None:
             dispatcher.utter_message("Seznam, ki ga želiš urediti, ne obstaja!")
@@ -1000,27 +997,24 @@ class ValidateListUpdateForm(FormValidationAction):
         else:
             list_name = list_name[0][1]
             list_id = list_name[0][0]
-        # if current_items is str:
-        #     items = [current_items]
+        if current_items is str:
+            items = [current_items]
         if re.search('odstrani ', name, re.IGNORECASE) or re.search('izbriši ', name, re.IGNORECASE):
             name = re.sub(r'odstrani ', '', name, flags=re.IGNORECASE)
             name = re.sub(r'izbriši ', '', name, flags=re.IGNORECASE)
-
-            res = list_helper.remove_from_list(list_name, name)
+            res = list_helper.remove_from_list(list_id, name)
             if res:
                 dispatcher.utter_message(f"Odstranil sem ˝{name}˝. Še kaj drugega?")
-                # items.remove(name)
-                # return {"list_item": None, "list_items": items}
-                return {"list_item": None, "list_items": None}
+                items.remove(name)
+                return {"list_item": None, "list_items": items}
             else:
                 dispatcher.utter_message(f"Elementa ˝{name}˝ ni na seznamu.")
-                return {"list_item": None, "list_items": None}
+                return {"list_item": None, "list_items": items}
 
         item = add_list_item(name, tracker.get_slot("list_name"), tracker.sender_id)
-        # items.append(item)
+        items.append(item)
         dispatcher.utter_message(f"Dodal sem ˝{item}˝. Še kaj drugega?")
-        return {"list_item": None, "list_items": None}
-        # return {"list_item": None, "list_items": items}
+        return {"list_item": None, "list_items": items}
 
     def validate_list_name(
             self,
@@ -1030,13 +1024,16 @@ class ValidateListUpdateForm(FormValidationAction):
             domain: DomainDict,
     ) -> Dict[Text, Any]:
         list_name = tracker.get_slot("list_name")
+        items = list_helper.get_list_items(list_name)
+        list_items = [item for itemname in items for item in itemname]
         if list_name:
+
             dispatcher.utter_message(f"Urejaš seznam {list_name}. Povej kaj želiš dodati ali odstraniti?")
-            return {"list_name": list_name}
+            return {"list_name": list_name, "list_items": list_items}
         list_name = tracker.latest_message.get("text")
         if list_helper.get_list(list_name, tracker.sender_id):
             dispatcher.utter_message(f"Urejaš seznam {list_name}. Povej kaj želiš dodati ali odstraniti?")
-            return {"list_name": list_name}
+            return {"list_name": list_name, "list_items": list_items}
         else:
             dispatcher.utter_message(f"Nisem našel seznama z imenom {list_name}.")
             return {"list_name": None}
